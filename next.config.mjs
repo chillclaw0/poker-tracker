f4c315b7855e64cfb23b2bcfc87449579578edb0
Chill Claw <chillclaw@Chills-iMac.local>
2026-04-27 17:30:40 -0700

    remove static export to enable client interactivity

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   assetPrefix: '/poker-tracker/',
   basePath: '/poker-tracker',
-  distDir: 'out',
 };
 
 export default nextConfig;
